5bf12dd5cb9d60172b6d23ba65920ccb19f9b3e5
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-07-27 15:16:00 +0300

    Add security-crypto dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation 'androidx.security:security-crypto:1.0.0'
 }
