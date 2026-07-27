28d8a5c99e6115fc7e20a40aeb2061259545d581
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-07-27 16:56:23 +0300

    Set default values for Device properties

--- app/src/main/java/com/example/netguardzte/domain/model/Device.kt
+++ app/src/main/java/com/example/netguardzte/domain/model/Device.kt
@@ -2,8 +2,8 @@ package com.example.netguardzte.domain.model
 
 data class Device(
     val mac: String,
-    val ip: String,
-    val hostname: String,
-    val connectionType: String,
+    val ip: String = "",
+    val hostname: String = "",
+    val connectionType: String = "WiFi",
     val isBlocked: Boolean = false
 )
